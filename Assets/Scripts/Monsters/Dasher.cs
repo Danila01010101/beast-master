80dc0f7d052e673c1dc9e1a5c6c3d6a9dead17e0
deminalexander <danila228337228@gmail.com>
2023-08-04 18:30:56 +0300

    New monsters foulder

---
+++ Assets/Scripts/Monsters/Dasher.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace BeastMaster
+{
+    public class Dasher : Monster
+    {
+        [SerializeField] private float _dashRange = 3.5f;
+
+        public void FixedUpdate()
+        {
+
+        }
+    }
+}
